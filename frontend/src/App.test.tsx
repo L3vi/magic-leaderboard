804a821de5b34638f856a6c1f7d7f4e1f35d9793
L3vi <levistum@gmail.com>
2025-08-18 19:45:33 -0600

    updating packages

--- frontend/src/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Magic Leaderboard title', () => {
-  render(<App />);
-  expect(screen.getByText(/Magic Leaderboard/i)).toBeInTheDocument();
-});
